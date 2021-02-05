67728e34a1ed3443ece29c64fd89f57fb12fb756
Nobel <nobarakat@gmail.com>
2021-02-05 10:36:45 -0700

    lets see it purple

--- config.h
+++ config.h
@@ -12,10 +12,10 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#9370DB";
-static const char col_cyan[]        = "#D65D0E";
-//static const char blue[]        = "#9370DB";
-static const char blue[]        = "#D65D0E";
+static const char col_cyan[]        = "#9370DB";
+//static const char col_cyan[]        = "#D65D0E";
+static const char blue[]        = "#9370DB";
+//static const char blue[]        = "#D65D0E";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
