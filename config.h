62ecc21340c947ed325f33ea941d9030702d552a
Nobel <nbarakat6@gatech.edu>
2021-02-28 21:15:10 -0700

    window swallowing

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -30,9 +31,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "libreoffice-impress",  NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "vlc",  NULL,       NULL,       NULL,       1,           -1 }
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "vlc",  NULL,       NULL,       NULL,       1,        -1,          0,           1,        -1},
+	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
@@ -61,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "/home/nobel/.cargo/bin/alacritty", NULL };
 static const char *flameshot[]  = { "flameshot", "gui", NULL };
 static const char *clipmenu[]  = { "/home/nobel/Suckless/clipmenu/clipmenu", "-i", NULL };
 static const char *micmute[]  = { "/home/nobel/Scripts/mutemic.sh", NULL };
