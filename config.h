2d5dee6562b8577a08f5a49e103fd06386a4ab78
Nobel <nbarakat6@gatech.edu>
2021-03-10 22:17:02 -0700

    suckless term

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "vlc",  NULL,       NULL,       NULL,       1,        -1,          0,           1,        -1},
-	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "/home/nobel/.cargo/bin/alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *flameshot[]  = { "flameshot", "gui", NULL };
 static const char *clipmenu[]  = { "/home/nobel/Suckless/clipmenu/clipmenu", "-i", NULL };
 static const char *micmute[]  = { "/home/nobel/Scripts/mutemic.sh", NULL };
