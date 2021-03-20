8262030df15b5626493d805a2c465ea64ff88a9e
Nobel Barakat <nbarakat6@gatech.edu>
2021-03-20 14:05:47 -0600

    theme and gaps

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int gappx            = 0;        
+static const int gappx            = 15;        
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
@@ -13,10 +13,10 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#9370DB";
-//static const char col_cyan[]        = "#D65D0E";
-static const char blue[]        = "#9370DB";
-//static const char blue[]        = "#D65D0E";
+//static const char col_cyan[]        = "#9370DB";
+static const char col_cyan[]        = "#D65D0E";
+//static const char blue[]        = "#9370DB";
+static const char blue[]        = "#D65D0E";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
